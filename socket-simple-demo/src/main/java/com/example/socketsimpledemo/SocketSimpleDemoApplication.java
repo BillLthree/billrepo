7c05ccbd43c524ff6f9ac2e6afb85342f2133ef4
BillLthree <1784442321@qq.com>
2022-11-01 17:50:36 +0800

    socket demo

---
+++ socket-simple-demo/src/main/java/com/example/socketsimpledemo/SocketSimpleDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.socketsimpledemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocketSimpleDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SocketSimpleDemoApplication.class, args);
+    }
+
+}
---
+++ socket-simple-demo/src/test/java/com/example/socketsimpledemo/SocketSimpleDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.socketsimpledemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocketSimpleDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
